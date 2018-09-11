185d86a4bcd5dbf7b88c72a9f43507d4aca7ab56
LastEndConductor <42978769+LastEndConductor@users.noreply.github.com>
2018-09-11 19:01:24 -0400

    changed the silly clown for a butterfly

--- exercises/exercise1/js/script.js
+++ exercises/exercise1/js/script.js
@@ -1,5 +1,5 @@
 // Exercise 1 - Moving pictures
-// Pippin Barr
+// Pippin barr
 //
 // Starter code for exercise 1.
 // It moves two pictures around on the canvas.
@@ -7,11 +7,11 @@
 // One moves toward the mouse cursor.
 
 
-// The image of a clown face
-var clownImage;
-// The current position of the clown face
-var clownImageX;
-var clownImageY;
+// The image of a butterfly face
+var butterflyImage;
+// The current position of the butterfly face
+var butterflyImageX;
+var butterflyImageY;
 
 //The image of the arrow
 var arrowImage
@@ -34,7 +34,7 @@ var feltTextureImageY;
 // Load the two images we're using before the program starts
 
 function preload() {
-  clownImage = loadImage("assets/images/clown.png");
+  butterflyImage = loadImage("assets/images/golden_butterfly.png");
   feltTextureImage = loadImage("assets/images/black-felt-texture.png");
   arrowImage = loadImage("assets/images/arrow.png")
 }
@@ -48,16 +48,16 @@ function setup() {
   // Create our canvas
   createCanvas(640,640);
 
-  // Start the clown image at the centre of the canvas
-  clownImageX = width/2;
-  clownImageY = height/2;
+  // Start the butterfly image at the centre of the canvas
+  butterflyImageX = width/2;
+  butterflyImageY = height/2;
 
   //setting arrow width and Height
   arrowWidth = 150;
   arrowHeight = 75;
 
   //start the arrow at the left of the screen in the center
-    arrowX = 0+arrowWidth/2;
+    arrowX = 0-arrowWidth/2;
     arrowY = height/2;
 
   // Start the felt image perfectly off screen above the canvas
@@ -72,7 +72,7 @@ function setup() {
 // draw()
 //
 // Moves the felt image linearly
-// Moves the clown face toward the current mouse location
+// Moves the butterfly face toward the current mouse location
 
 function draw() {
 
@@ -80,20 +80,20 @@ function draw() {
   feltTextureImageY += 1;
 
   //display and move arrow
-  image(arrowImage,arrowX+=5,arrowY,arrowWidth,arrowHeight);
+  image(arrowImage,arrowX+=2,arrowY,arrowWidth,arrowHeight);
 
   // Display the felt image
   image(feltTextureImage,feltTextureImageX,feltTextureImageY);
 
-  // Move the clown by moving it 1/10th of its current distance from the mouse
+  // Move the butterfly by moving it 1/10th of its current distance from the mouse
 
   // Calculate the distance in X and in Y
-  var xDistance = mouseX - clownImageX;
-  var yDistance = mouseY - clownImageY;
-  // Add 1/10th of the x and y distance to the clown's current (x,y) location
-  clownImageX = clownImageX + xDistance/10;
-  clownImageY = clownImageY + yDistance/10;
+  var xDistance = mouseX - butterflyImageX;
+  var yDistance = mouseY - butterflyImageY;
+  // Add 1/10th of the x and y distance to the butterfly's current (x,y) location
+  butterflyImageX = butterflyImageX + xDistance/10;
+  butterflyImageY = butterflyImageY + yDistance/10;
 
-  // Display the clown image
-  image(clownImage,clownImageX,clownImageY);
+  // Display the butterfly image
+  image(butterflyImage,butterflyImageX,butterflyImageY);
 }
