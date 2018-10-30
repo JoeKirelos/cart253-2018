ba267c0b9b978cc128208cd214b097cdd48badd1
LastEndConductor <42978769+LastEndConductor@users.noreply.github.com>
2018-10-30 15:24:33 -0400

    ex6 added descriptions to comments

--- exercises/exercise6/js/Paddle.js
+++ exercises/exercise6/js/Paddle.js
@@ -2,7 +2,7 @@
 //
 // A class that defines how a paddle behaves, including the ability
 // to specify the input keys to move it up and down
-///////////////////fixed
+///////////////////fixed commented out comments
 //Paddle constructor
 //Sets the properties with the provided arguments or defaults
 function Pladdle(x,y,w,h,speed,downKey,upKey) {
@@ -41,7 +41,7 @@ Paddle.prototype.update = function() {
 // display()
 //
 // Draw the paddle as a rectangle on the screen
-////////////////fixed
+////////////////fixed removed extra parenthesis 
 Paddle.prototype.disploy = function() {
   rectangle(this.x,this.y,this.w,this.h);
 }
