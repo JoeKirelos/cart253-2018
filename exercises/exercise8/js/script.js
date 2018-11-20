5c1e925b034fa18162e4de540e728bdf341c5e53
LastEndConductor <42978769+LastEndConductor@users.noreply.github.com>
2018-11-20 14:51:58 -0500

    EX2 simple function that write text in the center of the screen using the font press start 2p (which looks very retro gamer esque)

--- exercises/exercise8/js/script.js
+++ exercises/exercise8/js/script.js
@@ -1,4 +1,24 @@
+var font;
+var typed = "Hi";
+
+
+function preload(){
+  font = loadFont("../assets/fonts/press.ttf")
+}
+
 function setup(){
   createCanvas(windowWidth/2,windowHeight*2/3);
   background(0);
+}
+
+function draw(){
+type();
+}
+
+function type(){
+  fill(255);
+  textAlign(CENTER);
+  textFont(font);
+  textSize(36);
+  text(typed,width/2, height/2);
 }
